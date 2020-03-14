36a781a360eca3d12701ac3f3f4f28a5f9bd5633
Leow Wen Bin <dleowwb@gmail.com>
2020-03-14 12:01:40 +0800

    Create general framework for main script

---
+++ src/main.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+TODO_STRING = 'TODO'
+
+def find_todos(dir_path)
+    result = []
+    get_paths_in_dir(dir_path).each do |subpath|
+        if File.directory?(subpath)
+            result += find_todos(subpath)
+        elsif File.file?(subpath) && has_todos?(subpath)
+            result.append(
+                join_paths(
+                    dir_path,
+                    subpath
+                )  
+            )
+        else
+            raise "Path is neither directory nor file? (#{  "TODO get abs path"  })"
+        end
+    end
+
+    result
+end
+
+def join_paths(dir_path, file_path)
+end
+
+def get_paths_in_dir(dir_path)
+end
+
+def has_todos?(file_path)
+end
