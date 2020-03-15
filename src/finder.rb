e1a5ef490809be1cec7fe2046934354484a77220
Leow Wen Bin <dleowwb@gmail.com>
2020-03-15 11:00:37 +0800

    Modify code to operate on absolute file paths, so it's easier to understand

--- src/finder.rb
+++ src/finder.rb
@@ -1,33 +1,33 @@
 # frozen_string_literal: true
 
+# work only with abs paths
+
 TODO_STRING = 'TODO'
 
 def find_todos(dir_path)
+    find_todos_abs(
+        File.expand_path(
+            dir_path
+        )
+    )
+end
+
+def find_todos_abs(abs_dir_path)
     result = []
-    get_paths_in_dir(dir_path).each do |subpath|
-        if File.directory?(subpath)
-            result += find_todos(subpath)
-        elsif File.file?(subpath) && file_has_todos?(subpath)
+    Dir.entries(dir_path).each do |next_level_path|
+        raise "Subpath does not exist? (#{ next_level_path })" unless File.exist?(next_level_path)
+        if File.directory?(next_level_path)
+            result += find_todos_abs(next_level_path)
+        elsif File.file?(next_level_path) && file_has_todos?(next_level_path)
             result.append(
-                join_paths(
-                    dir_path,
-                    subpath
-                )  
+                next_level_path
             )
         else
-            raise "Path is neither directory nor file? (#{  "TODO get abs path"  })"
+            raise "Path is neither directory nor file? (#{next_level_path})"
         end
     end
 
     result
-end
-
-def join_paths(dir_path, file_path)
-    dir_path + file_path
-end
-
-def get_paths_in_dir(dir_path)
-    Dir.entries(dir_path)
 end
 
 def file_has_todos?(file_path)
