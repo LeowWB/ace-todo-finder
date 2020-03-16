976c04a006aa4abdd9f0f0b8ed25aa01146ccc72
Leow Wen Bin <dleowwb@gmail.com>
2020-03-16 15:19:08 +0800

    Refactor file_has_todos?

--- src/finder.rb
+++ src/finder.rb
@@ -11,6 +11,7 @@ class Finder
     def find_todos()
         raise "The path you provided doesn't exist: #{@dir_path}" unless File.exist?(@dir_path)
         raise "The path you provided isn't a directory: #{@dir_path}" unless File.directory?(@dir_path)
+
         find_todos_abs(
             File.expand_path(
                 @dir_path
@@ -21,7 +22,7 @@ class Finder
     def find_todos_abs(abs_dir_path)
         result = []
         Dir.children(abs_dir_path).each do |subpath|
-            next if subpath[0] == '.'        
+            next if subpath[0] == '.'
             next_level_path = File.join(abs_dir_path, subpath)        # TODO can replace with a map.
             raise "Subpath does not exist? (#{ next_level_path })" unless File.exist?(next_level_path)
             if File.directory?(next_level_path)
@@ -39,8 +40,13 @@ class Finder
     end
 
     def file_has_todos?(file_path)
-        File
-            .foreach(file_path)
+        any_line_has_todos?(
+            File.foreach(file_path)
+        )
+    end
+    
+    def any_line_has_todos?(lines_enumerator)
+        lines_enumerator
             .grep(TODO_REGEX)
             .any?
     end
