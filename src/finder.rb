ffc152d6dda9207f5dc7ee02e0dba0816311f399
Leow Wen Bin <dleowwb@gmail.com>
2020-03-16 13:31:48 +0800

    Fix issue where script crashes when reading file without TODO

--- src/finder.rb
+++ src/finder.rb
@@ -19,10 +19,10 @@ def find_todos_abs(abs_dir_path)
         raise "Subpath does not exist? (#{ next_level_path })" unless File.exist?(next_level_path)
         if File.directory?(next_level_path)
             result += find_todos_abs(next_level_path)
-        elsif File.file?(next_level_path) && file_has_todos?(next_level_path)
+        elsif File.file?(next_level_path)
             result.append(
                 next_level_path
-            )
+            ) if file_has_todos?(next_level_path)
         else
             raise "Path is neither directory nor file? (#{next_level_path})"
         end
