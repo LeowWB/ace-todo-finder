f5c5108a3d504c9718612c04e2f1166675b8a5bc
Leow Wen Bin <dleowwb@gmail.com>
2020-03-16 18:08:09 +0800

    Write tests for finder.file_has_todos?

--- test/spec/finder_spec.rb
+++ test/spec/finder_spec.rb
@@ -2,6 +2,8 @@
 
 require './src/finder.rb'
 
+TEST_CASE_DIR = "./test/test_cases/"
+
 describe Finder do
     
     describe '.any_line_has_todos?' do
@@ -92,7 +94,43 @@ describe Finder do
             end
         end
     end
+
+    describe '.file_has_todos?' do
+        context 'given file without todos' do
+            it 'returns false' do
+                file_has_todos_helper(
+                    TEST_CASE_DIR + "file_without_todo.txt",
+                    false
+                )
+            end
+        end
+
+        context 'given file with todos' do
+            it 'returns true' do
+                file_has_todos_helper(
+                    TEST_CASE_DIR + "file_with_todo.txt",
+                    true
+                )
+            end
+        end
+
+        context 'given file with invalid utf8 data' do
+            it 'does not give an error' do
+                expect do
+                    Finder.file_has_todos?(
+                        TEST_CASE_DIR + "invalid_utf8_file"
+                    )
+                end.not_to raise_error
+            end
+        end
+    end
 end
+
+def file_has_todos_helper (file_path, expected)
+    result = Finder.file_has_todos?(file_path)
+    expect(result).to be expected
+end
+
 
 def any_line_has_todos_test_helper (array, expected)
     result = Finder.any_line_has_todos?(array.each)
